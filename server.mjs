f8fb9bb5a76f406c5831ed7b6962305049b4ae5a
Zach <53745970+zach7815@users.noreply.github.com>
2022-12-31 19:28:00 +0000

    Cors Change

--- server.mjs
+++ server.mjs
@@ -1,7 +1,7 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 import express, { urlencoded } from "express";
-import cors from 'cors';
+// import cors from 'cors';
 import mongoose from "mongoose";
 import toDo from "./toDo.mjs";
 
@@ -14,7 +14,19 @@ const app = express();
 app.use(urlencoded({extended:true}));
 app.use(express.static('public'));
 app.use(express.json({limit:'1mb'}));
-app.use(cors);
+// app.use(cors);
+
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', "https://todo-app-backend-a4kl.onrender.com" );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+    });
+
+
+
 
 const PORT = process.env.PORT || 8000;
 
